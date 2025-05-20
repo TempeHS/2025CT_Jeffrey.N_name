7356a7d9de07e429b3f9aafa596949e0645b9a84
JeffreyNgo <jeffrey.ngo@education.nsw.gov.au>
2025-05-20 21:57:49 +1000

    .

---
+++ Assets/Scripts/MapTransition/VentMapTransitionTime.cs
@@ -0,0 +1,124 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VentMapTransitionTime : MonoBehaviour
+{
+    [Header("Map and Movement")]
+    [SerializeField] private PolygonCollider2D mapBoundary;
+    [SerializeField] private Direction direction1;
+    [SerializeField] private Direction direction2;
+    [SerializeField] private float additivePos1;
+    [SerializeField] private float additivePos2;
+
+    [Header("Vent Components")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private float VentCooldown;
+    [SerializeField] private float PenaltyTime;
+    public Collider2D collider1;
+    public Collider2D collider2;
+
+    PlayerMovement playerMovement;
+    TimerController timerController;
+    CinemachineConfiner confiner;
+
+    enum Direction { Up, Down, Left, Right };
+
+    private void Start()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        timerController = FindObjectOfType<TimerController>();
+    }
+
+    private void Awake()
+    {
+        confiner = FindObjectOfType<CinemachineConfiner>();
+        animator.GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            timerController.PenaltyApplied(PenaltyTime);
+            playerMovement.trailRenderer.time = 0;
+
+            confiner.m_BoundingShape2D = mapBoundary;
+            UpdatePlayerPosition(collision.gameObject);
+            UpdatePlayerPosition2(collision.gameObject);
+
+            MapController.Instance?.HighlightArea(mapBoundary.name);
+
+            if (animator != null) animator.SetBool("VentTrigger", true);
+
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction1)
+        {
+            case Direction.Up:
+                newPos.y += additivePos1;
+
+                break;
+
+            case Direction.Down:
+                newPos.y -= additivePos1;
+                break;
+
+            case Direction.Right:
+                newPos.x += additivePos1;
+                break;
+
+            case Direction.Left:
+                newPos.x -= additivePos1;
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+
+    private void UpdatePlayerPosition2(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction2)
+        {
+            case Direction.Up:
+                newPos.y += additivePos2;
+                break;
+
+            case Direction.Down:
+                newPos.y -= additivePos2;
+                break;
+
+            case Direction.Right:
+                newPos.x += additivePos2;
+                break;
+
+            case Direction.Left:
+                newPos.x -= additivePos2;
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+
+    IEnumerator Cooldown()
+    {
+        collider1.enabled = false;
+        collider2.enabled = false;
+
+        yield return new WaitForSeconds(VentCooldown);
+
+        animator.SetBool("VentTrigger", false);
+
+        collider1.enabled = true;
+        collider2.enabled = true;
+    }
+}
