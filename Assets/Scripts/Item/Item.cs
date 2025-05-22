02fe60521d4bdd44e734106c353286c5365f5e9a
JeffreyNgo <jeffrey.ngo@education.nsw.gov.au>
2025-05-22 15:04:45 +1000

    .

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -10,3 +10,37 @@ public class Item : MonoBehaviour
     }
 
 }
+
+// Create a new script with the following stuff
+/*
+public class Key : Item
+{
+    public override void UseItem()
+    {
+        // Code where the item would be used
+    }
+}
+
+Then in another script with box collider set on trigger and a child collider gameobject
+
+InventoryController inventoryController
+
+private void Awake()
+{
+    inventoryController = GetComponent<InventoryController>();
+}
+
+private void OnTriggerEnter2D(Collider2D collision)
+{
+    foreach (Transform child in Parent.transform)
+    {
+        if (child.name == "Childname")
+        {
+            GameObject childname;
+        }
+    }
+    if (collision.gameObject.CompareTag("Player"))
+}
+*/
+
+
